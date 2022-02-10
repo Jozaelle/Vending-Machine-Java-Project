d1e378054fa932b76410adfcde0003850586b5a3
Colin Davis <crdavis1997@yahoo.com>
2022-02-10 12:20:12 -0600

    added main menu

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class VendingMachineCLI {
 
 	public VendingMachineCLI() {
@@ -7,13 +9,21 @@ public class VendingMachineCLI {
 	}
 
 	public static void main(String[] args) {
+
 		VendingMachineCLI cli = new VendingMachineCLI();
+		// import inventory file and set up items
 		cli.run();
+
 	}
 
 	public void run() {
+		Scanner userInput = new Scanner(System.in);
+
 		displayWelcomeMessage();
+		mainMenuUI(userInput);
+
 		// ToDo - Add Code here to show menu, etc.
+		userInput.close();
 	}
 
 	public void displayWelcomeMessage(){
@@ -21,6 +31,51 @@ public class VendingMachineCLI {
 		System.out.println("*  Welcome to our vending machine application.");
 		System.out.println("*************************************************************");
 		System.out.println("");
+	}
+
+	public void mainMenuUI(Scanner userInput) {
+
+
+		boolean isValidChoice = false;
+		while (!isValidChoice) {
+			System.out.println("1. Display Items");
+			System.out.println("2. Purchase Items");
+			System.out.println("3. Exit");
+			System.out.println("Please enter a choice: ");
+			String userChoice = userInput.nextLine();
+			switch (userChoice) {
+				case "1":
+					// call DisplayItem
+					System.out.println("choice 1");
+					isValidChoice = true;
+					break;
+				case "2":
+					// display sub menu
+					System.out.println("choice 2");
+					isValidChoice = true;
+					break;
+				case "3":
+					// exit program
+					System.out.println("choice 3");
+					isValidChoice = true;
+					break;
+				case "4":
+					// generate sale report
+					System.out.println("generate sales report");
+					isValidChoice = true;
+					break;
+				default:
+					System.out.println("Not a valid entry");
+					break;
+			}
+		}
+		System.out.println("after while loop");
+//		if (userChoice.equals("1")){
+//			// call displayItem
+//		} else if ( userChoice.equals("2")){
+//			//display sub
+//		} else if
+
 	}
 
 }
